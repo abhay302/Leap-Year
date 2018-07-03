9be6784297c8f021eab39e123a7e10be4001c35b
abhay302 <39900609+abhay302@users.noreply.github.com>
2018-07-03 14:49:06 +0530

    Update Leap Year.kt

--- Leap Year.kt
+++ Leap Year.kt
@@ -1,18 +1,5 @@
-import java.util.*
-
 /**
- * isLeapYear() is an extension funtion to the Date class.
- * returns true if the year is a leap year or otherwise, returns false
- */
-fun Date.isLeapYear() : Boolean {
-    val c = Calendar.getInstance()
-    c.time = this
-    return c.get(Calendar.YEAR)%4==0
-}
-
-
-/**
- * Takes year as the argument
+ * The isLeapYear() function takes year as the argument
  * returns true if the year is a leap year or otherwise, returns false if it's not a leap year
  */
 fun isLeapYear(year : Int) = year%4==0
