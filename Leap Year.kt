d8fac717c2455abacdce6432b918e351b9f443d6
abhay302 <39900609+abhay302@users.noreply.github.com>
2018-07-05 10:22:02 +0530

    Update Leap Year.kt

--- Leap Year.kt
+++ Leap Year.kt
@@ -2,10 +2,7 @@
  * The isLeapYear() function takes year as the argument
  * returns true if the year is a leap year or otherwise, returns false if it's not a leap year
  */
-fun isLeapYear(year: Int) = when (year % 4) {
-    0 -> !(year % 100 == 0 && year % 400 != 0)
-    else -> false
-}
+fun isLeapYear(year: Int) = if (year % 4 == 0) !(year % 100 == 0 && year % 400 != 0) else false
 
 fun main(args: Array<String>) {
     print(isLeapYear(1700))
